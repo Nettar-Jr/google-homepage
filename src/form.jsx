d90ed6c397b6ebfc2aa3283ae4a901371e1a9b3f
Nettar-Jr <alabiabdulmumuni9@gmail.com>
2021-11-07 01:11:54 +1300

    put img in folder

--- src/form.jsx
+++ src/form.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import './form.css';
-import logo from './logo.png';
-import Search from './search.png';
-import Mic from './mic.png';
+import shLogo from './img/logo.png';
+import Search from './img/search.png';
+import Mic from './img/mic.png';
 
 function Input(){
 
-  const [state, setState] = useState(logo);
+  const [state, setState] = useState(shLogo);
 
   const showText = (props) => {
     console.log(props);
@@ -17,7 +17,7 @@ function Input(){
     <div className="form">
       <div className="logoContainer">
         <p>
-          {state ? state : <img src={logo}/>}
+          {state ? state : <img src={shLogo}/>}
         </p>
       </div>
       <div className="inputContainer">
