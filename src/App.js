46f1558d89c99057139a4e5b672485cf9e4d0e3c
grillzwitu <grillzwitu@gmail.com>
2021-11-07 18:53:21 +0100

    added top section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Main from './Main'
+import Top from './Top_section'
 
 function App() {
   return (
     <div className="App">
+      <Top />
       <Main />
     </div>
   );
---
+++ src/Top_section.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Top_section.css';
+import Menu from './img/baseline_apps_black_24dp.png';
+
+const Top =()=>{
+    return (
+        <header className="Top-header">
+
+            <div className="Top-menu-container">
+
+            <a
+                className="Top-menu-link"
+                href="https://mail.google.com/"
+                target="_top"
+            >
+                Gmail
+            </a>
+
+            <a
+                className="Top-menu-link"
+                href="https://www.google.com.ng/"
+                target="_top"
+            >
+                Images
+            </a>
+
+            <img src={Menu} alt="menu icon" className="Menu-icon"/>
+
+            <button className="Top-menu-link Sign-In-btn"><span className="Btn-text">Sign In</span></button>
+
+            </div>
+        
+        </header>
+    )
+}
+
+export default Top;
